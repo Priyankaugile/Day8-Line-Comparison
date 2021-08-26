53ab46b0b124327ede54751855c5e9d3c02461a9
priyankaugile <ugilepriyanka26@gmail.com>
2021-08-26 15:28:12 +0530

    Added: Display welcome message.

---
+++ LineComparison.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to Line\n" +
+                "Comparison Computation\n" +
+                "Program on Master Branch");
+    }
+}
